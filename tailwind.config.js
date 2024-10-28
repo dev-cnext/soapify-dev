4b1b984b4135d7c04031be191e4fcd822152a2a2
unknown <gurpreetsingh.cybernext@gmail.com>
2024-10-28 14:54:23 +0530

    blog template

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     './layout/*.liquid',
-    '.sections/*.liquid',
+    './sections/*.liquid',
     './snippets/*.liquid',
     './templates/customers/*.liquid',
     './templates/*.liquid'
